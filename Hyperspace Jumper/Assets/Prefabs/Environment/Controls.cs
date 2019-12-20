2626ace0fc2a1c0d192b2c3b2e3f0a2edcf5f4c6
Owen Merkling <omerkling@users.noreply.github.com>
2019-12-20 08:43:09 -0700

    File left out of previous commit

--- Hyperspace Jumper/Assets/Prefabs/Environment/Controls.cs
+++ Hyperspace Jumper/Assets/Prefabs/Environment/Controls.cs
@@ -1,14 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Controls : MonoBehaviour {
     public GameObject spaceship;
     public GameObject tunnel;
+    public GameObject restartButton;
+
+    bool canRestart;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    internal void ShowRestart() {
+        restartButton.SetActive(true);
+        canRestart = true;
     }
 
     // Update is called once per frame
@@ -17,11 +27,22 @@ public class Controls : MonoBehaviour {
             spaceship.GetComponent<Spaceship>().Jump();
         }
 
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            if (canRestart) {
+                Restart();
+            }
+        }
+
         float h = Input.GetAxisRaw("Horizontal");
         if (h > 0) {
             tunnel.GetComponent<TunnelMovement>().RotateClockwise();
         } else if (h < 0) {
             tunnel.GetComponent<TunnelMovement>().RotateCounterClockwise();
         }
+    }
+
+    public void Restart() {
+        print("restart");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
